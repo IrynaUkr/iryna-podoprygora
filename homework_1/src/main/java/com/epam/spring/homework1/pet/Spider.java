d809b514148a3737fe96957e262567f5a174cd2b
add <ukrira@gmail.com>
2022-06-25 19:07:01 +0300

    HW_1 has been added

---
+++ homework_1/src/main/java/com/epam/spring/homework1/pet/Spider.java
@@ -0,0 +1,14 @@
+package com.epam.spring.homework1.pet;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Spider implements Animal {
+    public Spider() {
+
+    }
+    @Override
+    public String toString() {
+        return "Spider";
+    }
+}
