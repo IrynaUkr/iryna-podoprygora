40b431299664c528d6136ad535f5c1e5a8d0b370
add <ukrira@gmail.com>
2022-07-04 22:34:02 +0300

    added Swagger Documentation to ProductController and UserController

--- homework-3/src/main/java/com/epam/cashier/controller/ProductController.java
+++ homework-3/src/main/java/com/epam/cashier/controller/ProductController.java
@@ -4,6 +4,10 @@ import com.epam.cashier.controller.dto.ProductDto;
 import com.epam.cashier.controller.dto.group.OnCreate;
 import com.epam.cashier.controller.dto.group.OnUpdate;
 import com.epam.cashier.controller.service.ProductService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,36 +17,47 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Api(tags = "API description for SWAGGER documentation Product")
+@ApiResponses({
+        @ApiResponse(code = 404, message = "Not found"),
+        @ApiResponse(code = 500, message = "Internal Server Error")
+})
 public class ProductController {
     private final ProductService productService;
 
+    @ApiOperation("get all products")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/product")
     public List<ProductDto> getAllproducts() {
         return productService.listProduct();
     }
 
+
+    @ApiOperation("create product")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/product")
     public ProductDto createProduct(@RequestBody @Validated(OnCreate.class) ProductDto productDto) {
         return productService.createProduct(productDto);
     }
 
+    @ApiOperation("update product")
     @ResponseStatus(HttpStatus.OK)
     @PutMapping(value = "/product/{id}")
     public ProductDto updateProduct(@PathVariable int id,
-                                    @RequestBody @Validated(OnUpdate.class) ProductDto productDto ) {
+                                    @RequestBody @Validated(OnUpdate.class) ProductDto productDto) {
         return productService.updateProduct(id, productDto);
     }
 
+    @ApiOperation("get product by id")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/product/{id}")
     public ProductDto getProduct(@PathVariable int id) {
         return productService.getProduct(id);
     }
 
-
+    @ApiOperation("delete product by id")
     @DeleteMapping("/product/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable int id) {
         productService.deleteProduct(id);
